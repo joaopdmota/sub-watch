e81e29c81ec643e2738e3f2ecfb232180324b59e
João Paulo Duarte Mota <jp.1998.jpd@icloud.com>
2025-12-08 11:47:47 -0300

    update otel boilerplate

--- boilerplate-go/infra/otel/otel.go
+++ boilerplate-go/infra/otel/otel.go
@@ -4,51 +4,67 @@ import (
 	"boilerplate-go/application/services"
 	"context"
 	"os"
+	"time"
 
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
 
 func Init(ctx context.Context, logger services.Logger) func() {
 	endpoint := os.Getenv("OTEL_EXPORTER_OTLP_ENDPOINT")
-
 	if endpoint == "" {
-		logger.Warn("[OTEL] OTEL_EXPORTER_OTLP_ENDPOINT empty, OTEL disabled")
-		return func() {}
+		endpoint = "otel-collector:4317"
+		logger.Warn("[OTEL] OTEL_EXPORTER_OTLP_ENDPOINT empty, using default: ", endpoint)
 	}
 
-conn, err := grpc.NewClient(
-		endpoint,
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	client := otlptracegrpc.NewClient(
+		otlptracegrpc.WithEndpoint(endpoint),
+		otlptracegrpc.WithInsecure(),
 	)
-	if err != nil {
-		logger.Error("[OTEL] failed to create gRPC client:", err)
-		return func() {}
-	}
 
-	exporter, err := otlptracegrpc.New(ctx, otlptracegrpc.WithGRPCConn(conn))
+	exporter, err := otlptrace.New(ctx, client)
 	if err != nil {
 		logger.Error("[OTEL] failed to create exporter: ", err)
 		return func() {}
 	}
 
+	serviceName := os.Getenv("OTEL_SERVICE_NAME")
+	if serviceName == "" {
+		serviceName = "cnpj-finder"
+	}
+
+	res, err := resource.New(
+		ctx,
+		resource.WithFromEnv(),
+		resource.WithAttributes(
+			semconv.ServiceNameKey.String(serviceName),
+		),
+	)
+	if err != nil {
+		logger.Error("[OTEL] failed to create resource: ", err)
+		return func() {}
+	}
+
 	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithResource(res),
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
 		sdktrace.WithBatcher(exporter),
-		sdktrace.WithResource(resource.NewWithAttributes(
-			semconv.SchemaURL,
-			semconv.ServiceName(os.Getenv("OTEL_SERVICE_NAME")),
-		)),
 	)
 
 	otel.SetTracerProvider(tp)
 
+	logger.Info("[OTEL] Init OK")
+
 	return func() {
 		logger.Info("[OTEL] Shutdown")
-		_ = tp.Shutdown(ctx)
+		ctxShutdown, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := tp.Shutdown(ctxShutdown); err != nil {
+			logger.Error("[OTEL] error on shutdown: ", err)
+		}
 	}
 }
